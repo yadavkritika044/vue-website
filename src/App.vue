680b98bdf1f8721ec8892c8ebd9936da13830d14
yadavkritika044 <72384488+yadavkritika044@users.noreply.github.com>
2020-10-05 16:00:54 +0530

    changes in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
+    <HelloWorld msg="Hi I am Kritika Yadav"/>
   </div>
 </template>
 
