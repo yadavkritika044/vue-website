f5e1a32078e866e0640477c8365b74987da0deab
yadavkritika044 <72384488+yadavkritika044@users.noreply.github.com>
2020-10-08 16:13:04 +0530

    added profile image

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <Navigation />
+    <Lander />
   </div>
 </template>
 
 <script>
 import Navigation from "./components/includes/nav.vue"; //import the component
-
+import Lander from "./components/Lander.vue";
 export default {
   name: "App",
   components: {
     Navigation, //instantiate it here
+    Lander,
   },
 };
 </script>
---
+++ src/components/Lander.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container-fliud">
+        <img alt="Vue logo" class="rounded-circle" :src="tanuprofilepic" />
+        <h1>Software Developer</h1>
+        <p>This is a fun project I built while learning vue.js and bootstrap</p>
+        <button type="button" class="btn btn-primary btn-lg">Contact</button>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+import tanuprofilepic from "../assets/tanuVueProfile.jpg";
+export default {
+  data: function () {
+    return {
+      tanuprofilepic: tanuprofilepic,
+    };
+  },
+};
+</script>
+<style lang="css" scoped>
+.section {
+  padding-top: 10%;
+  padding-bottom: 10%;
+}
+</style>
